92995fbc33a59569db5666d2ca8591733795f1d2
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-05 14:56:02 +0400

    add and test react webcam

---
+++ src/app/[locale]/detect/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Webcam from "react-webcam";
+export default function Detect(){
+
+    return(
+        <div>
+            <div>
+                <Webcam/>
+            </div>
+        </div>
+    )
+}
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
     <div>
-      <h1 className='bg-white'>{t('title')}</h1>
+      <h1 className='bg-white text-2xl'>{t('title')}</h1>
       <Link href="/about">{t('about')}</Link>
     </div>
   );
