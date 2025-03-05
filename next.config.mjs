a231d4b612c6ab42b63a6938729d792ab0a1aa8c
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-05 14:14:41 +0400

    install dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+};
+ 
+export default withNextIntl(nextConfig);
