7de8c8ccd8dbff2968993adfa2b0a3ab055365db
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-06 12:22:23 +0400

    adding a navbar

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[12vh] flex flex-col items-center justify-center w-screen bg-amber-50'>
+        SNAP BIN
+    </div>
+  )
+}
+
+export default Navbar
