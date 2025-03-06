95955bca7b40866fd2a6fb1830e4ac5a76e93d93
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-06 23:03:04 +0400

    remove borders

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   const t = useTranslations('HomePage');
   return (
     <div className='h-fit flex flex-col items-center '>
-      <div className='h-[70vh]  border-x border-gray-300 w-full'>
+      <div className='h-[70vh] bg-blue-500 rounded-4xl w-full'>
         <h1 className='bg-white text-6xl font-bold m-2'>{t('title')}</h1>
         <div className='bg-amber-200 rounded-3xl p-2 w-fit'>
           <Link className='text-5xl ' href="/detect">{t('detect')}</Link>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='h-[12vh] text-3xl flex flex-col items-center justify-center w-full border-x border-b  border-gray-300 '>
+    <div className='h-[12vh] text-3xl flex flex-col items-center justify-center w-full  '>
         SNAP BIN
     </div>
   )
