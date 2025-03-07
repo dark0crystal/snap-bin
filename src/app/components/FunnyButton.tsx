160d089d25d4bc734e5528ebb2bdb751056859cd
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-08 03:01:05 +0400

    styling

--- src/app/components/FunnyButton.tsx
+++ src/app/components/FunnyButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Link from "next/link";
+import { Link } from "@/i18n/routing";
 import { useTranslations } from "next-intl";
 
 
@@ -12,11 +12,11 @@ export default function FunnyButton() {
     <motion.div
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
-      className="bg-amber-200 rounded-3xl px-4 py-2 cursor-pointer w-20 md:w-30 flex items-center justify-center"
+      className=" rounded-3xl px-4 py-2 cursor-pointer w-20 md:w-30 flex items-center justify-center mt-6"
     >
       <Link href="/detect">
         <motion.span
-          className="text-lg sm:text-2xl md:text-4xl font-semibold text-[#f9f5ec] flex"
+          className="text-xl sm:text-2xl md:text-4xl font-semibold text-[#f9f5ec] flex tracking-wider"
           initial={{ x: 0 }}
           animate={{ x: [0, -5, 5, -5, 5, 0] }} // Funny shake animation
           transition={{ duration: 0.4, repeat: Infinity, repeatType: "reverse" }}
