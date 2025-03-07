75ab34c9b8465895dce4d914863ac49c5884b08c
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-07 14:41:27 +0400

    fix

--- src/app/[locale]/detect/page.tsx
+++ src/app/[locale]/detect/page.tsx
@@ -107,7 +107,7 @@ export default function Detect() {
     <div className="flex flex-col items-center  max-h-[90vh]  rounded-3xl p-4 ">
       <div className="w-[90vw] md:w-[80vw] lg:w-[50vw] h-[80vh] flex flex-col items-center justify-center  relative overflow-hidden">
         {/* Camera simulation */}
-        <div className="w-full h-[80vh] rounded-2xl overflow-hidden bg-black flex justify-center items-center">
+        <div className="w-full h-[75vh] rounded-2xl overflow-hidden bg-black flex justify-center items-center">
           {image ? (
             // captured image section 
             <div className="w-full overflow-hidden relative h-full">
