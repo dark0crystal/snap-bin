7e8da0ced380287fe1be8a56848d7dd42f8bc654
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-07 13:15:30 +0400

    navbar link brand

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LanguageChange from './LangChange'
 
 import { Phudu } from "next/font/google";
+import { Link } from '@/i18n/routing';
 
 const phudu = Phudu({
   subsets: ["latin"],
@@ -18,7 +19,7 @@ const Navbar =async () => {
     <div className='h-[12vh] text-3xl grid grid-cols-2 sm:grid-cols-3 items-center justify-center w-full  '>
       <LanguageChange/>
       <div className='flex items-center justify-center w-full '>
-        <h1 className={`${phudu.className} font-semibold text-3xl sm:text-4xl`}>SNAP BIN</h1>
+        <Link href="/" className={`${phudu.className} font-semibold text-3xl sm:text-4xl`}>SNAP BIN</Link>
       </div>
         
       <div className='hidden sm:flex'/>
