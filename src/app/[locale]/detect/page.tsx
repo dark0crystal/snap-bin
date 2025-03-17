d2541153ad9e53dde5d8b35a5706ec22e1e4b9be
ALMardasDEV <albusaidi9094@gmail.com>
2025-03-18 01:51:13 +0400

    fix

--- src/app/[locale]/detect/page.tsx
+++ src/app/[locale]/detect/page.tsx
@@ -166,7 +166,7 @@ export default function Detect() {
                 onClick={retakePhoto}
                 className="px-6 py-2 bg-red-500 text-white rounded-full w-40 hover:bg-red-600 transition"
               >
-                Retake Photo
+                {t("retake")}
               </button>
 
               {/* Show detected objects */}
